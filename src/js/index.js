857b5dd65eaaf27530a0d65c4582f6cae6937707
chelovek-18 <chelovek-18@yandex.ru>
2017-09-24 04:25:35 +0400

    Configurate mosquito and include modules

--- src/js/index.js
+++ src/js/index.js
@@ -5,39 +5,47 @@
 import { lang } from "./lang/lang";
 import { test } from "./test/test";
 const
-    subj = { lang: lang, test: test };
-
-/*window.mosqito = new (
-    function Mosquito( param ) {
-        param = param || {};
-
-        //this.oldBrowserMessage = typeof param.oldBrowserMessage == 'function' ? param.oldBrowserMessage : oldBrowserMessage;
-        if ( !Object.assign ) return subj.test.oldBrowserMessage();
-
-        //Object.assign( this, param );
-        
-        this.rename = function() {
-            Object.keys( window ).forEach( function( n ) { if ( window[ n ] instanceof Mosquito ) delete window[ n ]; } );
-            return new Mosquito;
-        };
-    }
-)( window.mosqito );*/
+    modules = { lang: lang, test: test };
 
 // ------------------------------------- Create mosqito -------------------------------------
-window.mosqito = new (
+window.mosquito = new (
     class Mosquito
     {
-        // Configurated mosqito and include modules
-        constructor( conf = {} ) {
-            /*if ( param && param instanceof Array )
-                Object.keys( subj ).forEach( ( m ) => { if ( !~param.indexOf( m ) ) delete subj[ m ]; } );
-            Object.assign( this, subj );*/
+        // Configurate mosqito and include modules
+        constructor( conf ) {
+            // Configurate
+            if ( conf && typeof conf == 'object' ) {
+                // Formatting config object
+                Object.assign(
+                    conf = conf instanceof Array ? { on: conf } : conf,
+                    { on: conf.on || Object.keys( modules ), off: conf.off || [] }
+                );
+                // Modules on/off
+                Object.keys( modules ).forEach( m => {
+                    if ( !~conf.on.indexOf( m ) || ~conf.off.indexOf( m ) ) delete modules[ m ];
+                });
+                // Redefine modules methods with user config
+                Object.keys( conf )
+                    .filter( m => m != 'on' && m != 'off' )
+                    .forEach( m => {
+                        if ( modules[ m ] )
+                            Object.keys( m ).forEach( ( method, methName ) => modules[ methName ] = method );
+                    });
+                // Add plugs for uninstall modules
+                this.lang = {
+                    t: function( string ) { return string; }
+                }
+            }
+            // Include modules
+            Object.assign( this, modules );
         }
 
-        // For rename: let newName = mosqito.rename();
+        // Use: let newName = mosqito.rename();
         rename() {
-            Object.keys( window ).forEach( n => window[ n ] instanceof Mosquito && delete window[ n ] );
-            return new Mosquito;
+            // Delete old object:
+            Object.keys( window ).forEach( n => { if ( window[ n ] instanceof Mosquito ) delete window[ n ]; } );
+            return this;
         }
+
     }
-)( window.mosqito );
+)( window.mosquito );
