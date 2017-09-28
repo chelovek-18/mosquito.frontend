4136ceb26caa1457a5d24f63a89258194cc480f3
chelovek-18 <chelovek-18@yandex.ru>
2017-09-28 15:01:46 +0400

    Class Lang and descriptions

--- src/js/lang/lang.js
+++ src/js/lang/lang.js
@@ -34,7 +34,95 @@ let
     };
 
 // ------------------------------------- Language -------------------------------------
-export let lang = {
+/**
+ * @class Lang
+ * Translate string with local json file or online service
+ * 
+ * 
+ * @constructor
+ * Set default language by function getLang()
+ * Create functions .t() and .$m_t() in String prototype for translate by: "my-string".t( [language] );
+ * 
+ * @method localization set         Set current language
+ * @param {String} lng                  --> Language ('en', 'ru', etc.)
+ * 
+ * @method localization get         Get current language
+ * @returns {String}                    <-- Language ('en', 'ru', etc.)
+ * 
+ * @method addLocalizations         Add localizations in current json
+ * @param {Object} obj                  --> Json { "Hi": "Привет" } <- or -> { "ru": { "Hi": "Привет" } }
+ * @param {?String} lng                 --> Language ('en', 'ru', etc.) <- or -> undefined
+ * 
+ * @method getLocalizations         Get current json localizations
+ * @param {?String} lng                 --> Language ('en', 'ru', etc.) <- or -> undefined
+ * @returns {Object}                    <-- Json language
+ * 
+ * @method onlineTranslateOn        On/off online translate and set translate service
+ * @param {?Function/Boolean} fn        --> Translate function <- or -> undefined (set default function) <- or -> false/null for online translate off
+ * @returns {String}                    <-- Translated string
+ * 
+ * @method t                        Translate
+ * @param {String} string               --> String for translate
+ * @param {?String} lng                 --> Language ('en', 'ru', etc.) <- or -> undefined
+ * @returns {String}                    <-- Translated string
+ * 
+ */
+
+export let lang = new (
+    class Lang
+    {
+        constructor() {
+            let self = this;
+
+            localization = getLang();
+            String.prototype.t = function( lng ) {
+                return self.t( this.toString(), lng );
+            }
+            String.prototype.$m_t = function( lng ) {
+                return self.t( this.toString(), lng );
+            }
+        }
+
+        set localization( lng ) {
+            if ( languages[ lng ] || lng == 'en' ) localization = lng;
+        }
+
+        get localization() {
+            return localization;
+        }
+
+        addLocalizations( obj, lng ) {
+            if ( lng ) languages[ lng ] = Object.assign( languages[ lng ] ? languages[ lng ] : {}, obj );
+            else languages = Object.assign( languages, obj );
+        }
+
+        getLocalizations( lng ) {
+            return lng ? languages[ lng ] : languages;
+        }
+
+        // Translate by online-service or use json files
+        // TODO: ActiveXObject and account in microsofttranslator.com
+        // TODO: Use mosquito method instead of xhr
+        // TODO: Set Azure key
+        onlineTranslateOn( fn ) {
+            onlineTranslate = fn === undefined ? function( string, lng ) {
+                var xhr = new XMLHttpRequest();
+                xhr.open( 'GET', location.protocol + '//api.microsofttranslator.com/V2/Ajax.svc/Translate?text=' + string + '&appId=E8DB680F742769E3F9B95BFDB55798C13FEB0E5C&to=' + lng, false );
+                xhr.send();
+                if ( xhr.status != 200 || /ArgumentOutOfRangeException/.test( xhr.responseText ) ) return string;
+                else return xhr.responseText;
+            } : fn;
+        }
+
+        // Translate
+        t( string, lng ) {
+            lng = lng || localization;
+            if ( onlineTranslate ) return onlineTranslate( string, lng );
+            return ( ( lng != 'en' && languages[ lng ] ) ? languages[ lng ][ string ] : null ) || string;
+        }
+    }
+);
+/*export let lang = {
     // Get/set current language ('en', 'ru', etc.)
     set localization( lng ) {
         if ( languages[ lng ] || lng == 'en' ) localization = lng;
@@ -47,6 +135,26 @@ export let lang = {
     // Translate by online-service or use json files
     onlineTranslate: function( on ) {
         onlineTranslate = on === undefined ? true : on;
+    },
+
+    onlineTranslateObj: {
+        vendor: 'microsoft',
+        key: 'E8DB680F742769E3F9B95BFDB55798C13FEB0E5C',
+    },
+
+    onlineTranslateApi: new function() {
+        this.request = function( url, method = 'GET', isAsync = false ) {
+            let xhr = new XMLHttpRequest();
+            xhr.open( method, url, isAsync );
+            xhr.send();
+            return xhr;
+            //if ( xhr.status != 200 || /ArgumentOutOfRangeException/.test( xhr.responseText ) ) return string;
+            //else return xhr.responseText;
+        }
+        this.microsoft = function( lng, string, appId ) {
+            let url = location.protocol + '//api.microsofttranslator.com/V2/Ajax.svc/Translate?text=' + string + '&appId=' + lang.onlineTranslateObj.key + '&to=' + lng;
+            return this.request( url );
+        }
     },
 
     // Add/get json localizations object
@@ -62,12 +170,17 @@ export let lang = {
     // Translate
     // TODO: ActiveXObject and account in microsofttranslator.com
     // TODO: Use mosquito method instead of xhr
+    // TODO: Set Azure key
     t: function( string, lng ) {
         lng = lng || localization;
         if ( onlineTranslate ) {
-            var xhr = new XMLHttpRequest();
+            //var xhr = window.ActiveXObject ? new  ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();   // new ActiveXObject("Msxml2.XMLHTTP");          
+            /*var xhr = new XMLHttpRequest(); //0caf266e805a4704bde3d00679b705ef //6ff4e6bce4834e999061eea101f21074 //E8DB680F742769E3F9B95BFDB55798C13FEB0E5C //dead2627ba694b0abd01788a2bc21bcf
             xhr.open( 'GET', location.protocol + '//api.microsofttranslator.com/V2/Ajax.svc/Translate?text=' + string + '&appId=E8DB680F742769E3F9B95BFDB55798C13FEB0E5C&to=' + lng, false );
             xhr.send();
+            if ( xhr.status != 200 || /ArgumentOutOfRangeException/.test( xhr.responseText ) ) return string;
+            else return xhr.responseText;*-/
+            let xhr = lang.onlineTranslateApi[ lang.onlineTranslateObj.vendor ]( lng, string );
             if ( xhr.status != 200 || /ArgumentOutOfRangeException/.test( xhr.responseText ) ) return string;
             else return xhr.responseText;
         }
@@ -84,4 +197,4 @@ export let lang = {
         }
         setTimeout( function() { delete lang.init; }, 0 );
     })()
-}
+}*/
